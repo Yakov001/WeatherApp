dd1acbd30151ca99298390070d4a35546ed62c1e
yakov <yakovrussian@gmail.com>
2022-01-19 00:42:54 +0300

    Hid API keys

--- app/src/main/java/com/example/weather_app/retrofit/StringAPI.kt
+++ app/src/main/java/com/example/weather_app/retrofit/StringAPI.kt
@@ -2,13 +2,14 @@ package com.example.weather_app.retrofit
 
 import com.example.weather_app.model.Forecast
 import com.example.weather_app.utils.Consts.ADDITION_URL
+import com.example.weather_app.utils.Consts.WEATHER_API_KEY
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface StringAPI {
 
-    @GET(ADDITION_URL)
+    @GET(ADDITION_URL + WEATHER_API_KEY)
     suspend fun getWeather(
         @Query("lat") lat : Int = 60,
         @Query("lon") long : Int = 30
--- app/src/main/java/com/example/weather_app/utils/Consts.kt
+++ app/src/main/java/com/example/weather_app/utils/Consts.kt
@@ -1,10 +1,12 @@
 package com.example.weather_app.utils
 
+import com.example.weather_app.BuildConfig
+
 object Consts {
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 
     const val ADDITION_URL = "onecall?exclude=daily,current,minutely,alerts" +
-            "&appid=8ee48cb9a17352ed8d265326c0cd515d&units=metric"
+            "&units=metric&appid="
 
-    const val MAP_API_KEY = "AIzaSyBfjyBbIPLicxklAEdCHI5EEiCw2QvoPJo"
+    const val WEATHER_API_KEY = BuildConfig.WEATHER_API_KEY
 }
